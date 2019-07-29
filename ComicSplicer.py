8d1f231a0054517142967ee372d5fc6cc39ab472
James Kersey <jim.kersey@gmail.com>
2019-07-29 14:34:41 -0500

    saving spreads

--- ComicSplicer.py
+++ ComicSplicer.py
@@ -1,5 +1,9 @@
 # OUTPUT SETTINGS
+import getopt
 import os
+import sys
+
+from PIL import Image
 
 DPI = 300
 PAGE_WIDTH = 5.5
@@ -14,19 +18,50 @@ CROP_BOTTOM = 0
 
 class ComicSplicer:
 
-    mypath = ""
+    input_dir = ""
+    output_dir = ""
 
-    def __init__(self, mypath):
-        self.mypath = mypath
+    page_spread = ""
+
+    def __init__(self, input_dir, output_dir):
+
+        if not output_dir:
+            output_dir = input_dir + "_out"
+
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+
+        self.page_spread = Image.new(
+            "RGB", (int(PAGE_WIDTH * DPI * 2), int(PAGE_HEIGHT * DPI))
+        )
+
+        print(f"reading from {input_dir}, writing to {output_dir}")
+
+        self.process_pages()
+        self.process_cover()
+
+
+    def process_pages(self):
+
         files = self.get_page_list()
         extra_pages = len(files) % 4
         if extra_pages:
-            print(
-                f"warning: number of pages is not divisible by 4, { extra_pages } blank page(s) will be added."
-            )
-        self.print_spliced_images(files)
+            print("warning: number of pages is not divisible by 4")
+            print("bailing out.")
+            sys.exit(2)
+        self.splice_all_pages(files)
 
-    def print_spliced_images(self, files):
+    def process_cover(self):
+
+        files = self.get_page_list()
+        extra_pages = len(files) % 4
+        if extra_pages:
+            print("warning: number of pages is not divisible by 4")
+            print("bailing out.")
+            sys.exit(2)
+        self.splice_all_pages(files)
+
+    def splice_all_pages(self, files):
 
         num_pages = len(files)
         flip = False
@@ -36,15 +71,64 @@ class ComicSplicer:
                 a, b = i, -(i + 1)
             else:
                 a, b = -(i + 1), i
-            print(files[a], files[b])
+            self.splice_pages(files[a], files[b])
             flip = not flip
 
     def get_page_list(self):
-        image_path = self.mypath + os.sep + "images"
-        print(image_path)
-        (_, _, filenames) = next(os.walk(image_path))
+        page_path = self.input_dir + os.sep + "pages"
+        filenames = []
+        print(page_path)
+        for _, _, filenames in os.walk(page_path):
+            filenames = [f for f in filenames if not f[0] == "."]
+        print(f"got { len(filenames) } pages.")
         return filenames
+
+    def splice_pages(self, left_page, right_page):
+        left_image = Image.open(self.input_dir + os.sep + "pages" + os.sep + left_page)
+        right_image = Image.open(
+            self.input_dir + os.sep + "pages" + os.sep + right_page
+        )
+
+        left_image = left_image.resize(
+            (int(PAGE_WIDTH * DPI), int(PAGE_HEIGHT * DPI)), resample=Image.BILINEAR
+        )
+        right_image = right_image.resize(
+            (int(PAGE_WIDTH * DPI), int(PAGE_HEIGHT * DPI)), resample=Image.BILINEAR
+        )
+
+        self.page_spread.paste(left_image, (0, 0))
+        self.page_spread.paste(right_image, (int(PAGE_WIDTH * DPI), 0))
+
+        self.page_spread.save(
+            self.output_dir + os.sep + "pages" + os.sep + "_spread_" + left_page
+        )
+        # left_image.save(self.output_dir + os.sep + "pages" + os.sep + left_page)
+        # right_image.save(self.output_dir + os.sep + "pages" + os.sep + right_page)
+        print(left_page, " ", right_page)
+
+
+def main(argv):
+    input_dir = ""
+    output_dir = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["idir=", "odir="])
+    except getopt.GetoptError:
+        print("ComicSplicer.py -i <input_dir> -o <output_dir>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("test.py -i <input_dir> -o <output_dir>")
+            sys.exit()
+        elif opt in ("-i", "--idir"):
+            input_dir = arg
+        elif opt in ("-o", "--odir"):
+            output_dir = arg
+    print("Input directory is ", input_dir)
+    print("Output file is ", output_dir)
+
+    ComicSplicer(input_dir, output_dir)
 
 
 if __name__ == "__main__":
-    comic_splicer = ComicSplicer(".")
+    main(sys.argv[1:])
